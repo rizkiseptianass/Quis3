5f0af024a0e09aa255af309286d7a3429c096aea
Rizki Septianas <rizkiseptianass@gmail.com>
2021-03-28 18:49:04 +0700

    project pertama

---
+++ app/src/main/java/com/example/quis3/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.quis3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    val id:Int = 10
+    val language:String = "kotlin"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener() {
+            intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("id_value", id)
+            intent.putExtra("language_value", language)
+            startActivity(intent)
+
+        }
+    }
+}
